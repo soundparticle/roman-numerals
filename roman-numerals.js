ef2556abf41485c961e38dbfc89be54ffda041fe
Antreo Pukay <acrosswalker@yahoo.com>
2019-03-31 14:37:39 -0700

    last single digit test pass

--- roman-numerals.js
+++ roman-numerals.js
@@ -6,6 +6,9 @@ function toRoman(num) {
   if(num === 2) return 'II';
   if(num === 3) return 'III';
   if(num === 4) return 'IV';
+  if(num === 5) return 'V';
+  if(num == 6) return 'VI';
+  if(num == 9) return 'IX';
   return num;
   console.log('* num *', num);
 }
